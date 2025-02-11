cd7af8c784d6f94e67cafcc957b927305b5de7d6
a1phaki <jok.jok.875@gmail.com>
2025-02-12 03:17:09 +0800

    demo上傳

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/C02_QT/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/C02_QT_demo/" : "/",
   plugins: [react()],
 });
