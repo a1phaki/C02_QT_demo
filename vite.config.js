0c7f89ef8521b8da17e801e494d3f83c2a76514f
a1phaki <jok.jok.875@gmail.com>
2025-02-12 10:27:46 +0800

    修改圖片路徑，把/移掉

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/C02_QT_demo/" ,
+  base: process.env.NODE_ENV === "production" ? "/C02_QT_demo/" : "/",
   plugins: [react()],
 });
